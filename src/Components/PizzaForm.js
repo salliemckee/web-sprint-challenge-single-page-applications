5aca7541107ca1b3b84af888c70581dcc693d42c
Sallie McKee <salliemckee@Morrigan.local>
2022-02-25 14:18:23 -0600

    added toppings checklist

--- src/Components/PizzaForm.js
+++ src/Components/PizzaForm.js
@@ -19,12 +19,29 @@ const PizzaForm = () => {
         </label>
         <label>
           &nbsp;Pizza Size&nbsp;
-          <select
-            id="size-dropdown"
-            name="size"
-            type="dropdown"
-            placeholder="Select pizza size"
-          ></select>
+          <select id="size-dropdown" name="size" type="dropdown">
+            <option value="">---Select Pizza Size---</option>
+            <option value="Small">Small</option>
+            <option value="Medium">Medium</option>
+            <option value="Large">Large</option>
+          </select>
+        </label>
+        <h4>Select Toppings Here:</h4>
+        <label>
+          &nbsp;Onions&nbsp;
+          <input name="onions" type="checkbox"></input>
+        </label>
+        <label>
+          &nbsp;Tomatos&nbsp;
+          <input name="tomatoes" type="checkbox"></input>
+        </label>
+        <label>
+          &nbsp;Bell Peppers&nbsp;
+          <input name="bellPeppers" type="checkbox"></input>
+        </label>
+        <label>
+          &nbsp;Roasted Garlic&nbsp;
+          <input name="roastedGarlic" type="checkbox"></input>
         </label>
       </div>
     </form>
